9c51b9900a5411be186af5d16954aef6c899d15a
lipe <filipe@yardim.com.br>
2021-05-16 21:06:15 -0300

    melhorando configurações do sturtup

--- UnitOfWork/Startup.cs
+++ UnitOfWork/Startup.cs
@@ -40,11 +40,8 @@ namespace UnitOfWork
 
             app.UseHttpsRedirection();
 
-            // Enable middleware to serve generated Swagger as a JSON endpoint.
             app.UseSwagger();
 
-            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
-            // specifying the Swagger JSON endpoint.
             app.UseSwaggerUI(c =>
             {
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
